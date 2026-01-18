81976230fa099d37edde0d6cba1f8a0dc4ab1f8c
Marcos Elias <marechek@gmail.com>
2026-01-18 20:46:24 -0300

    Se agregan las carpetas/archivos styles y js

---
+++ js/auth.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $('#loginForm').on('submit', function (e) {
+        e.preventDefault();
+
+        const email = $('#email').val().trim();
+        const password = $('#password').val().trim();
+
+        if (email === '' || password === '') {
+            showMessage('Todos los campos son obligatorios', 'danger');
+            return;
+        }
+
+        // Datos para simulacion de login
+        const userEmail = 'admin@alkewallet.cl';
+        const userPassword = '1234';
+
+        if (email === userEmail && password === userPassword) {
+            showMessage('Login exitoso. Redirigiendo...', 'success');
+
+            localStorage.setItem('loggedUser', email);
+
+            setTimeout(() => {
+                window.location.href = 'menu.html';
+            }, 1000);
+
+        } else {
+            showMessage('Credenciales incorrectas', 'danger');
+        }
+    });
+
+    function showMessage(message, type) {
+        $('#loginMessage').html(`
+            <div class="alert alert-${type}" role="alert">
+                ${message}
+            </div>
+        `);
+    }
+
+});
